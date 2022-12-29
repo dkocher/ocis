1e202e4354de7a89b9d1fc098b7aec9e07ebabcd
jkoberg <jkoberg@owncloud.com>
2022-12-29 15:49:28 +0100

    stream per user
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- services/hub/pkg/service/sse.go
+++ services/hub/pkg/service/sse.go
@@ -2,20 +2,17 @@ package service
 
 import (
 	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/cs3org/reva/v2/pkg/ctx"
 	"github.com/go-chi/chi/v5"
 	"github.com/r3labs/sse/v2"
-	"net/http"
-	"time"
 )
 
-type SSE struct{}
-
+// ServeSSE provides server sent events functionality
 func ServeSSE(r chi.Router) {
 	server := sse.New()
-	stream := server.CreateStream("messages")
-	stream.AutoReplay = false
-
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		u, ok := ctx.ContextGetUser(r.Context())
 		if !ok {
@@ -23,14 +20,28 @@ func ServeSSE(r chi.Router) {
 			return
 		}
 
+		uid := u.GetId().GetOpaqueId()
+		if uid == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		stream := server.CreateStream(uid)
+		stream.AutoReplay = false
+
 		go func() {
 			for range time.Tick(time.Second * 4) {
 				t := time.Now()
-				server.Publish("messages", &sse.Event{
+				server.Publish(uid, &sse.Event{
 					Data: []byte(fmt.Sprintf("[%s] Hello %s, new push notification from server!", t.Format("2006-01-02 15:04:05"), u.Username)),
 				})
 			}
 		}()
+
+		// add stream to URL
+		q := r.URL.Query()
+		q.Set("stream", uid)
+		r.URL.RawQuery = q.Encode()
 
 		server.ServeHTTP(w, r)
 	})
